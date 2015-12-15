e3460c0859e6aeea02a86de64722d664271cecf3
Dan Steinberg <daniel_steinberg@intuit.com>
2015-12-15 11:31:45 -0800

    Added styling and functional main menu

--- dev/js/components/Profile.js
+++ dev/js/components/Profile.js
@@ -1,12 +1,32 @@
+/** @jsx React.DOM */
 var React = require("react");
 var ReactDOM = require("react-dom");
 
 var Profile = React.createClass({
 
 	render: function() {
+
+		var person = this.props.person;
+		var backgroundStyle = {
+            backgroundImage: 'url(' + person.imageUrl + ')'
+        };
+
 		return (
-			<div>
-				Profile goes here
+			<div className="profile">
+				<div className="profile-background" style={backgroundStyle}></div>
+				<div className="profile-text-box">
+					<img className="profile-image" src={person.pictureUrl} alt="Profile Image"/>
+					<div className="profile-name">{person.name}</div>
+					<div className="profile-text">Works at {person.company}</div>
+					<div className="profile-text">Attended {person.school}</div>
+					<div className="profile-text">Lives in {person.city}, {person.state}</div>
+
+					<div className="profile-stats">
+						<span className="profile-followers">{person.followers}</span>
+						&nbsp;followers |&nbsp;
+						<span className="profile-views">{person.visits}</span> views
+					</div>
+				</div>
 			</div>
 		)
 	}
--- dev/js/components/ProfilePage.js
+++ dev/js/components/ProfilePage.js
@@ -1,12 +1,29 @@
+/** @jsx React.DOM */
 var React = require("react");
 var ReactDOM = require("react-dom");
 
+var Profile = require("./Profile.js");
+var MainMenu = require("./MainMenu.js");
+
 var ProfilePage = React.createClass({
+
+	mainMenuItems: ["About", "Posts", "Collections", "Photos", "YouTube", "+1's", "Reviews"],
 
 	render: function() {
 		return (
-			<div>
-				Profile Page goes here
+			<div className="profile-page">
+				<div className="profile-page-top">
+					<div className="profile-page-top-left">
+						<Profile person={this.props.person}/>
+					</div>
+					<div className="profile-page-top-right">
+						<img src={this.props.person.imageUrl} alt="Profile Image Goes Here"
+							className="profile-page-image"/>
+					</div>
+				</div>
+				<div className="profile-page-bottom">
+					<MainMenu mainMenuItems={this.mainMenuItems}/>
+				</div>
 			</div>
 		)
 	}
